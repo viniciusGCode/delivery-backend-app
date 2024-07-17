7ead7050101d946fb835adbe2c20a6206ab6ee23
UT0P1C <ljonham@gmail.com>
2024-07-17 20:08:17 -0300

    Creating Vandors and adding database

--- controllers/AdminController.ts
+++ controllers/AdminController.ts
@@ -1,13 +1,39 @@
 import { Request, Response, NextFunction } from "express";
 import { CreateVandorInput } from "../dto";
+import { Vandor } from "../models";
 
 export const CreateVandor = async (
   req: Request,
   res: Response,
   next: NextFunction
 ) => {
-  const {name, address, pincode, foodType, email, password, ownerName, phone} = <CreateVandorInput>req.body
+  const {
+    name,
+    address,
+    pincode,
+    foodType,
+    email,
+    password,
+    ownerName,
+    phone,
+  } = <CreateVandorInput>req.body;
 
+  const createdVandor = await Vandor.create({
+    name,
+    address,
+    pincode,
+    foodType,
+    email,
+    password,
+    salt: "123213",
+    ownerName,
+    phone,
+    rating: 0,
+    serviceAvailable: false,
+    coverImages: [],
+  });
+
+  return res.json({ createdVandor });
 };
 
 export const GetVanndors = async (
@@ -20,4 +46,6 @@ export const GetVandorByID = async (
   req: Request,
   res: Response,
   next: NextFunction
-) => {};
+) => {
+  return res.send("hello world");
+};
--- index.ts
+++ index.ts
@@ -1,17 +1,26 @@
 import express from "express";
-import bodyParser from "body-parser";
-
 import { AdminRoute, VandorRoute } from "./routes";
+import mongoose from "mongoose";
+import { MONGO_URI } from "./config";
 
 const app = express();
 
-app.use(bodyParser)
-app.use(bodyParser.urlencoded({extended: true}))
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 app.use("/admin", AdminRoute);
 app.use("/vandor", VandorRoute);
 
+mongoose
+  .connect(MONGO_URI)
+  .then((result) => {
+    console.log("connected to database");
+  })
+  .catch((error) => {
+    console.error(error);
+  });
+
 app.listen(1337, () => {
-  console.clear()
+  console.clear();
   console.log("application running");
 });
